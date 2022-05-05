71ce2639b0b7a18a22fe90b627fe892154e58e9c
SevenTimes <krab47@yahoo.com>
2022-05-06 01:28:54 +0400

    chore: fix typo in regex

--- src/Components/Killer/KillerAddons.js
+++ src/Components/Killer/KillerAddons.js
@@ -4,7 +4,7 @@ const KillerAddons = (props) => {
 	return (
 		<div className="killer-addons">
 			{addons.map((addon) => {
-				const addonName = addon.name.replace(/[":]+g/, '');
+				const addonName = addon.name.replace(/[":]+/g, '');
 				return (
 					<img
 						src={`./Assets/Killers/Characters/${char}/Addons/${addonName}.png`}
